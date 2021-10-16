ebd35ea2d542a48099f3649bd260ef7dc44af2fd
Isaiah186 <91351729+Isaiah186@users.noreply.github.com>
2021-10-16 18:40:34 -0500

    Add files via upload

---
+++ Square Returner.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[21]:
+
+
+class RootReturn:
+    def __init__(self):
+        pass
+    
+    
+    def root(self):
+        x = int(input("Pick a number"))
+        print(x*x)
+
+
+# In[22]:
+
+
+RootReturn_obj = RootReturn()
+
+
+# In[23]:
+
+
+RootReturn_obj.root()
+
+
+# In[ ]:
+
+
+
+
